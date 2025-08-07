7bd999fdc8805ebc7030d097d6bbb5c11c6c5c55
Ezra Clintoc <140189732+CodKid1710@users.noreply.github.com>
2025-08-07 06:27:35 -0500

    Update pusher-auth.js

--- api/pusher-auth.js
+++ api/pusher-auth.js
@@ -1,3 +1,5 @@
+import Pusher from "pusher";
+
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
@@ -6,3 +8,10 @@ const pusher = new Pusher({
   useTLS: true,
 });
 
+export default function handler(req, res) {
+  const socketId = req.body.socket_id;
+  const channelName = req.body.channel_name;
+
+  const auth = pusher.authenticate(socketId, channelName);
+  res.status(200).send(auth);
+}
