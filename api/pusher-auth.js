88054b26d788d27035d90907624d8b358b66f6e3
Ezra Clintoc <140189732+CodKid1710@users.noreply.github.com>
2025-08-07 06:16:21 -0500

    Update pusher-auth.js

--- api/pusher-auth.js
+++ api/pusher-auth.js
@@ -1,1 +1,8 @@
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  useTLS: true,
+});
 
